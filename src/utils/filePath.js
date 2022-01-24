97c637aaeb3911cca8e567f063b07f7d46208c95
takiyweb <kanytakiy@gmail.com>
2022-01-24 20:20:19 +0000

    Changed the file path url

--- src/utils/filePath.js
+++ src/utils/filePath.js
@@ -1,6 +1,9 @@
 import config from "../config.json";
 
-const baseUrl = process.env.REACT_APP_BASE_URL || config.api.baseURL;
+const baseUrl =
+  "https://bibleschool-backend-lbpvo.ondigitalocean.app/api" ||
+  process.env.REACT_APP_BASE_URL ||
+  config.api.baseURL;
 const filesBaseURL = baseUrl.replace("/api", "");
 
 export const getFilePath = (path) => {
